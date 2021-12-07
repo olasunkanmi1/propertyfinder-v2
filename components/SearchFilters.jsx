34b3f4e6036541f3462d51d4808bc7763834f5fc
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-12-07 16:13:32 -0100

    Update Search Functionalities

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -3,13 +3,44 @@ import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-u
 import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
+import { filterData, getFilterValues } from '../utils/filterData'
 
 export default function SearchFilters() {
+    const router = useRouter();
+    const [filters, setfilters] = useState(filterData)
+
+    const searchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
+
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value
+        })
+
+        router.push({ pathname: path, query })
+
+    }
+
     return (
-        <Box>
-            <Flex>
-                ok
-            </Flex>
-        </Box>
+        <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+            {filters.map((filter) => (
+                <Box key={filter.queryName}>
+                    <Select 
+                        placeholder={filter.placeholder}
+                        w='fit-content'
+                        p='2'
+                        onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}
+                    >
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
     )
 }
