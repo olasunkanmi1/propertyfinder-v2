d7e39e7bf94e9afc046f2501885d97cfca4b94f4
Olasunkanmi <abdulsalamquadri999@gmail.com>
2021-12-07 16:24:28 -0100

    Update Search Functionalities

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,7 @@ import Property from "../components/property";
 import noResult from "../public/noresult.jpg";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 
-export default function search({ properties }) {
+export default function Search({ properties }) {
   const [searchFilters, setSearchFilters] = useState(false);
   const router = useRouter();
   return (
