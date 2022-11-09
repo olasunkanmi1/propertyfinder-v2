5569bb9cb191bc3b4782d8a70432450dcf79a1bc
Olasunkanmi <abdulsalamquadri999@gmail.com>
2022-11-09 17:47:53 +0100

    fixed build error

--- components/find-property-page/search-filters/mobile/filterbar/property-type/layout.tsx
+++ components/find-property-page/search-filters/mobile/filterbar/property-type/layout.tsx
@@ -9,8 +9,19 @@ import * as Gi from 'react-icons/gi';
 import * as Si from 'react-icons/si';
 import { IconType } from 'react-icons/lib';
 import { IPropertyType } from '.';
+import { useRecoilState } from 'recoil'
+import { filterAtom } from '../../../../../../states';
 
 const PropertyTypeLayout: React.FC<IPropertyType> = ({ list }) => {
+  const [filterState, setFilterState] = useRecoilState(filterAtom);
+  const { categoryExternalID: active } = filterState;
+
+  const changeTab = (value: string) => {
+    setFilterState(filterState => ({
+        ...filterState,
+        categoryExternalID: value,
+      }))
+  }
   return (
     <div className='overflow-auto'>
         { list?.map((type) => {
@@ -22,17 +33,17 @@ const PropertyTypeLayout: React.FC<IPropertyType> = ({ list }) => {
                     const { name, value, icon } = item;
 
                     return (
-                      <div key={name} className='flex flex-col items-center w-fit p-2'>
-                        <div className="flex items-center justify-center rounded-full w-10 h-10 border text-gray-500">
+                      <div onClick={() => changeTab(value)} key={name} className={`flex flex-col items-center w-fit p-2 ${active === value && 'text-primary font-bold'}`}>
+                        <div className={`flex items-center justify-center rounded-full w-10 h-10 border text-gray-500 ${active === value && 'bg-primary bg-opacity-20 border border-primary text-primary'}`}>
                             {
-                                icon?.slice(0, 2) === "Fa" ? React.createElement(Fa[icon as keyof IconType], {className: 'w-5 h-5'}) : 
-                                icon?.slice(0, 2) === "Md" ? React.createElement(Md[icon as keyof IconType], {className: 'w-5 h-5'}) :         
-                                icon?.slice(0, 2) === "Im" ? React.createElement(Im[icon as keyof IconType], {className: 'w-5 h-5'}) : 
-                                icon?.slice(0, 2) === "Io" ? React.createElement(Io[icon as keyof IconType], {className: 'w-5 h-5'}) : 
-                                icon?.slice(0, 2) === "Tb" ? React.createElement(Tb[icon as keyof IconType], {className: 'w-5 h-5'}) : 
-                                icon?.slice(0, 2) === "Hi" ? React.createElement(Hi[icon as keyof IconType], {className: 'w-5 h-5'}) :
-                                icon?.slice(0, 2) === "Gi" ? React.createElement(Gi[icon as keyof IconType], {className: 'w-5 h-5'}) :
-                                icon?.slice(0, 2) === "Si" ? React.createElement(Si[icon as keyof IconType], {className: 'w-5 h-5'}) : null
+                                icon?.slice(0, 2) === "Fa" ? React.createElement(Fa[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) : 
+                                icon?.slice(0, 2) === "Md" ? React.createElement(Md[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) :         
+                                icon?.slice(0, 2) === "Im" ? React.createElement(Im[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) : 
+                                icon?.slice(0, 2) === "Io" ? React.createElement(Io[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) : 
+                                icon?.slice(0, 2) === "Tb" ? React.createElement(Tb[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) : 
+                                icon?.slice(0, 2) === "Hi" ? React.createElement(Hi[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) :
+                                icon?.slice(0, 2) === "Gi" ? React.createElement(Gi[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) :
+                                icon?.slice(0, 2) === "Si" ? React.createElement(Si[icon as keyof IconType], {className: `propertyTypeIcon ${active === value && 'text-primary'}`}) : null
                             }
                         </div>
                         <span className='text-center text-sm w-max'> {name} </span>
