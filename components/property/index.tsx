861e4eddbd65e553dce43f4a25440eba2744a4d2
Olasunkanmi <abdulsalamquadri999@gmail.com>
2022-12-07 17:29:53 +0100

    fixed [next-auth][error][CLIENT_FETCH_ERROR] and designed saved-properties page

--- components/property/index.tsx
+++ components/property/index.tsx
@@ -5,52 +5,93 @@ import { PropertyProps } from '../../types'
 import DefaultImage from '../../public/assets/house.webp';
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
-import { FaBath, FaBed, FaRegHeart } from 'react-icons/fa';
+import { FaBath, FaBed } from 'react-icons/fa';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 import { MdWindow } from 'react-icons/md';
+import { db, storage } from '../../firebase'
+import { addDoc, collection, doc, serverTimestamp } from 'firebase/firestore'
+import { ref } from 'firebase/storage'
+import { useSession } from 'next-auth/react'
+import { useSetRecoilState } from 'recoil';
+import { navbarState } from '../../states';
 
 const Property: React.FC<PropertyProps> = ({ property }) => {
+  const { data: session, status } = useSession();
+  const setModal = useSetRecoilState(navbarState);
+
+//   console.log(session)
     const { coverPhoto, price, rooms, title, baths, area, isVerified, rentFrequency, agency, externalID } = property
 
+    const handleClick = async () => {
+        // alert('saved')
+        if(session) {
+            await addDoc(collection(db, "saved-properties"), {
+                userId: session?.user?.email,
+                coverPhoto, price, rooms, title, baths, area, isVerified, rentFrequency, agency, externalID,
+                timestamp: serverTimestamp(),
+            })
+        } else {
+            setModal( modal => ({
+                ...modal,
+                signInModal: true,
+            }))
+        }
+    }
+
     return (
-        <Link href={`/property/${externalID}`} passHref>
-            <a className="w-full ls:w-[300px] z-0">
-                <div className="relative rounded-xl w-full h-[160px] overflow-hidden">
-                    <Image 
-                        src={coverPhoto ? coverPhoto.url : DefaultImage} alt="cover-photo" layout="fill" priority
-                        blurDataURL={coverPhoto && coverPhoto.url} 
-                    />
-
-                    <div className="flex justify-center items-center absolute top-2 right-2 text-white z-20 hover:bg-gray-400 transition ease-in-out w-[30px] h-[30px] rounded-md">
-                        <FaRegHeart size={20} onClick={() =>  alert('done')} />
+        <div className="w-full ls:w-[300px]">
+            <Link href={`/property/${externalID}`} passHref>
+                <a className="w-full">
+                    <div className="relative rounded-xl w-full h-[160px] overflow-hidden">
+                        <Image 
+                            src={coverPhoto ? coverPhoto.url : DefaultImage} alt="cover-photo" layout="fill" priority
+                            blurDataURL={coverPhoto && coverPhoto.url} 
+                        />
                     </div>
+                </a>
+            </Link>
+
+            <div className="flex flex-col pr-2">
+                <div className="flex justify-between items-center">
+                    <Link href={`/property/${externalID}`} passHref>
+                        <a className='w-[calc(100%-35px)] h-[38px] pt-2 pl-2'>
+                            <div className="flex space-x-2 items-center h-full">
+                                { isVerified && <div className='text-green-500 '> <GoVerified size={20} /> </div> }
+                                <p className="font-bold font-lg leading-tight"> AED {millify(price)} {rentFrequency && ` ${rentFrequency}`} </p>
+                            </div>
+                        </a>
+                    </Link>
+                    
+                    <button className="flex justify-center items-center mt-2 bg-primary bg-opacity-50 cursor-pointer transition ease-in-out w-[30px] h-[30px] rounded-md" onClick={() =>  handleClick()}>
+                        <AiOutlineHeart size={20} color='#fff' />
+                        {/* <AiFillHeart size={20} color='rgb(255, 0, 0)' /> */}
+                    </button>
                 </div>
 
-                <div className="flex flex-col p-2 gap-[5px]">
-                    <div className="flex justify-between items-center">
-                        <div className="flex space-x-2 items-center">
-                            { isVerified && <div className='text-green-500 '> <GoVerified size={20} /> </div> }
-                            <p className="font-bold font-lg leading-tight"> AED {millify(price)} {rentFrequency && ` ${rentFrequency}`} </p>
+                <Link href={`/property/${externalID}`} passHref>
+                    <a className='pt-[5px]'>
+                        <div className="flex justify-between items-center font-normal">
+                            <p className='propertyBBA'> <FaBed />{rooms} </p>
+                            <p className='propertyBBA'> <FaBath />{baths} </p>
+                            <p className='propertyBBA'> <MdWindow />{millify(area)} sqft  </p>
                         </div>
-                        
-                        <div className="flex space-x-2 justify-center items-center w-[25px] h-[25px] relative rounded-full overflow-hidden border bg-secondary text-white">
-                            <Image 
-                                src={agency ? agency.logo.url : 'https://i.ibb.co/6vv08Pk/homr-removebg-preview.png'} alt="Agency Logo" layout="fill" priority 
-                                blurDataURL={agency ? agency.logo.url : 'https://i.ibb.co/6vv08Pk/homr-removebg-preview.png'}
-                            />
+
+                        <p className='truncate font-medium'> {title} </p>
+
+                        <div className='flex justify-between items-center'>
+                            { agency && <p className='truncate font-normal text-sm text-secondary w-[calc(100%-30px)]'> {agency.name} </p>}
+
+                            <div className="flex space-x-2 justify-center items-center w-[25px] h-[25px] relative rounded-full overflow-hidden border bg-secondary text-white">
+                                <Image 
+                                    src={agency ? agency.logo.url : 'https://i.ibb.co/6vv08Pk/homr-removebg-preview.png'} alt="Agency Logo" layout="fill" priority 
+                                    blurDataURL={agency ? agency.logo.url : 'https://i.ibb.co/6vv08Pk/homr-removebg-preview.png'}
+                                />
+                            </div>
                         </div>
-                    </div>
-
-                    <div className="flex justify-between items-center font-normal">
-                        <p className='propertyBBA'> <FaBed />{rooms} </p>
-                        <p className='propertyBBA'> <FaBath />{baths} </p>
-                        <p className='propertyBBA'> <MdWindow />{millify(area)} sqft  </p>
-                    </div>
-
-                    <p className='truncate font-medium'> {title} </p>
-                    { agency && <p className='truncate font-normal text-sm text-secondary'> {agency.name} </p>}
-                </div>
-            </a>
-        </Link>
+                    </a>
+                </Link>
+            </div>
+        </div>
   )
 }
 
--- pages/saved-properties.tsx
+++ pages/saved-properties.tsx
@@ -1,20 +1,60 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { GetServerSideProps } from 'next';
-import { Layout } from '../components'
-import Router from "next/router";
+import { Layout, Property } from '../components'
+import Router, { useRouter } from "next/router";
 import { useSetRecoilState } from 'recoil';
 import { loadingState } from '../states';
 import { SavedPropertiesPageProps } from '../types';
-import { getProviders } from 'next-auth/react';
+import { getProviders, useSession } from 'next-auth/react';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import { db } from '../firebase'
+import Heading from '../components/heading';
+import { Loader } from '../components/loader';
 
 const SavedProperties: React.FC<SavedPropertiesPageProps> = ({providers}) => {
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
   const setLoading = useSetRecoilState(loadingState);
   Router.events.on("routeChangeStart", () => setLoading(loading => ({...loading, routeChangeLoading: true})) );
   Router.events.on("routeChangeComplete", () => setLoading({propertiesLoading: false, routeChangeLoading: false}) );
+
+  const [savedProperties, setSavedProperties] = useState<any[]>([]);
+  const filteredProperties = savedProperties.filter((filter) => filter.data().userId = session?.user?.email)
+
+  useEffect(() => {
+    if(!session) router.push('/')
+  }, [session, router])
+
+  useEffect(() => onSnapshot(
+    query(collection(db, "saved-properties"), orderBy("timestamp", "desc")), //descending
+      (snapshot) => {
+        // console.log('properties', snapshot.docs[0].data())
+        setSavedProperties(snapshot.docs)
+      }
+  )
+, [])
+  
+  // console.log('properties', savedProperties)
+
   
   return (
     <Layout title='View your saved properties' providers={providers}>
-        <h1>Saved Properties</h1>
+      { session ? (
+        <div>
+          <Heading heading={`${filteredProperties.length >= 1 ? 'Saved Properties' : 'You have no saved properties'}`} />
+
+          <div className='flex flex-wrap gap-x-5 gap-y-10 w-full justify-center py-5'>
+            { filteredProperties.map((property) => {
+                return (
+                  <Property property={property.data()} key={property.id} />
+                )
+            })}
+          </div>
+        </div>
+      ) : (
+        <Loader />
+      ) }
     </Layout>
   )
 }
